c3ae3fd517d3e6818c346534be12c04df8d5d968
swastik3616 <swastikkulkarni737@gmail.com>
2024-01-28 16:06:34 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,12 +6,12 @@ def message_probability(user_message, recognised_words, single_response=False, r
     message_certainty = 0
     has_required_words = True
 
-    # Counts how many words are present in each predefined message
+
     for word in user_message:
         if word in recognised_words:
             message_certainty += 1
 
-    # Calculates the percent of recognised words in a user message
+    
     percentage = float(message_certainty) / float(len(recognised_words))
 
     # Checks that the required words are in the string
